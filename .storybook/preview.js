ac5b7f2b69cf9494d9917456ed96dc8e0a99d50e
Marc Dingena <marc.dingena@gmail.com>
2020-03-15 21:10:45 +1100

    Add introduction stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,8 +8,6 @@ addDecorator(storyFn => <Provider>{storyFn()}</Provider>);
 
 addParameters({
   options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
     theme: themes.dark
   },
 });
