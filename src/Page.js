91d9a92ac2b806c4041dbac272298cbc6fb34074
Marc Dingena <marc.dingena@gmail.com>
2020-03-16 21:25:01 +1100

    Add more examples for chapter 3

--- src/Page.js
+++ src/Page.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { useResizeObserver } from '@envato/react-breakpoints'
 import styles from './Page.module.css'
 
-const Page = ({ width = '100%', children }) => {
+const Page = ({ width = '100%', padding = '10px', children }) => {
   const [ref, observedEntry] = useResizeObserver({ box: 'border-box' })
 
+  const paddingLeft = padding
+  const paddingRight = padding
   const px = observedEntry?.borderBoxSize[0].inlineSize
 
   return (
@@ -14,7 +16,13 @@ const Page = ({ width = '100%', children }) => {
       style={{ width }}
     >
       <div className={styles.width}>{px}px</div>
-      <div className={styles.wrap}>
+      <div
+        className={styles.wrap}
+        style={{
+          paddingLeft,
+          paddingRight
+        }}
+      >
         {children}
       </div>
     </div>
---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Sidebar.module.css'
+
+const Sidebar = () => (
+  <div className={styles.root}>
+    <a href="#">Homepage</a>
+    <a href="#">Design</a>
+    <a href="#">Web Design</a>
+    <a href="#">Video &amp; Audio</a>
+    <a href="#">Marketing</a>
+    <a href="#">Inside Envato</a>
+  </div>
+)
+
+export default Sidebar
