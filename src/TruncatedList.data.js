de2779da4123fbd0efa7335fdbc7905bbb9c06a4
Marc Dingena <marc.dingena@gmail.com>
2020-03-18 11:42:28 +1100

    Add TruncatedList demo

---
+++ src/TruncatedList.data.js
@@ -0,0 +1,140 @@
+export default [{
+  id: 'Vertical',
+  href: '#Vertical',
+  text: 'Vertical'
+},
+{
+  id: 'Music',
+  href: '#Music',
+  text: 'Music'
+},
+{
+  id: 'Drone',
+  href: '#Drone',
+  text: 'Drone'
+},
+{
+  id: 'Backgrounds',
+  href: '#Backgrounds',
+  text: 'Backgrounds'
+},
+{
+  id: 'Reptile',
+  href: '#Reptile',
+  text: 'Reptile'
+},
+{
+  id: 'Double Exposure',
+  href: '#Double Exposure',
+  text: 'Double Exposure'
+},
+{
+  id: 'City',
+  href: '#City',
+  text: 'City'
+},
+{
+  id: 'Sea',
+  href: '#Sea',
+  text: 'Sea'
+},
+{
+  id: 'Sky',
+  href: '#Sky',
+  text: 'Sky'
+},
+{
+  id: 'People',
+  href: '#People',
+  text: 'People'
+},
+{
+  id: 'Dance',
+  href: '#Dance',
+  text: 'Dance'
+},
+{
+  id: 'Nature',
+  href: '#Nature',
+  text: 'Nature'
+},
+{
+  id: 'Beach',
+  href: '#Beach',
+  text: 'Beach'
+},
+{
+  id: 'Work',
+  href: '#Work',
+  text: 'Work'
+},
+{
+  id: 'Coffee',
+  href: '#Coffee',
+  text: 'Coffee'
+},
+{
+  id: 'Car',
+  href: '#Car',
+  text: 'Car'
+},
+{
+  id: 'Food',
+  href: '#Food',
+  text: 'Food'
+},
+{
+  id: 'Water',
+  href: '#Water',
+  text: 'Water'
+},
+{
+  id: 'Night',
+  href: '#Night',
+  text: 'Night'
+},
+{
+  id: 'Fire',
+  href: '#Fire',
+  text: 'Fire'
+},
+{
+  id: 'Cat',
+  href: '#Cat',
+  text: 'Cat'
+},
+{
+  id: 'Dog',
+  href: '#Dog',
+  text: 'Dog'
+},
+{
+  id: 'Love',
+  href: '#Love',
+  text: 'Love'
+},
+{
+  id: 'Business',
+  href: '#Business',
+  text: 'Business'
+},
+{
+  id: 'Snow',
+  href: '#Snow',
+  text: 'Snow'
+},
+{
+  id: 'Rain',
+  href: '#Rain',
+  text: 'Rain'
+},
+{
+  id: 'Woman',
+  href: '#Woman',
+  text: 'Woman'
+},
+{
+  id: 'Flower',
+  href: '#Flower',
+  text: 'Flower'
+}]
